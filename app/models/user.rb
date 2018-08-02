0ee8ea2c101a2be66529987aecbdc1b3e935eeb7
Vishal Sharma <vsh1819@gmail.com>
2018-08-02 09:15:16 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
 
   enum role: [:normal, :moderator, :admin]
 
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
   has_many :notes
   has_many :viewers
   has_many :readable, through: :viewers, source: :note
