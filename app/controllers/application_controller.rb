bd3b06f42be58c8fac1fa3dd3b2fd7fafbb919c4
Vishal Sharma <vsh1819@gmail.com>
2018-08-02 09:20:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  private
+
+ def user_not_authorized(exception)
+   flash[:error] = "Access denied."
+   redirect_to(request.referrer || root_path)
+ end
+
 end
